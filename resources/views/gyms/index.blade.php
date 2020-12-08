1f919301e62c8a388ec6a818be4f86c8199648d6
smileyuichi <smile.yuichi.1013@gmail.com>
2020-12-08 10:56:59 +0900

    implement:shopページを実装

--- resources/views/gyms/index.blade.php
+++ resources/views/gyms/index.blade.php
@@ -1,17 +1,53 @@
 @extends('layouts.application')
 @section('main-content')
-<div class="ready-wrapper">
-    <table>
-
-
-        @foreach($items as $item)
-        <tr>
-            <td>{{$item->name}}</td>
-
-        </tr>
-        @endforeach
-
-
-    </table>
+パンくずリスト
+@include('gyms.searched_heading')
+ぺじネーション
+<div class="shop-list">
+    @foreach($items as $item)
+    <div class="shop">
+        <a href="/gyms/show?id={{$item->id}}">
+            <div class="top-info">
+                <div class="top-info__name_and_area">
+                    <div class="top-info__name_and_area__name">{{$item->name}}</div>
+                    <div class="top-info__name_and_area__area">大阪府 / フィットネスクラブ</div>
+                </div>
+            </div>
+            <div class="bottom-info">
+                <div class="bottom-info-left">
+                    <div class="bottom-info-left__top-cover">
+                        <img src="" class="top-cover-img shop-img">
+                    </div>
+                </div>
+                <div class="bottom-info-right">
+                    <div class="bottom-info-right__overview">
+                        {{$item->outline}}
+                    </div>
+                    <div class="bottom-info-right__rating">
+                        <div class="star-rating" id="star-rate-40"></div>
+                        <div class="shop-rating">
+                            4.0
+                        </div>
+                        <div class="shop-count"><i class='fas fa-comment-dots'></i>1件</div>
+                        <div class='shop-count'><i class='fas fa-bookmark fa-lg'></i>0件</div>
+                    </div>
+                    <div class="bottom-info-right__top-wom">
+                        <div class="wom-info">
+                            <div class="wom-info__title">
+                                ヴィンテージの質良し
+                            </div>
+                            <div class="wom-info__user">
+                                test user
+                            </div>
+                        </div>
+                        <div class="wom-content">
+                            置いてある商品の質はどれもよかったです！欲を言えばもう少しお手頃価格だとありがたい…笑
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+    @endforeach
 </div>
 @endsection
---
+++ resources/views/gyms/searched_heading.blade.php
@@ -0,0 +1,16 @@
+<div class="searched_heading-box">
+    <div class="searched_heading-box__result">
+        <span class="searched_heading-box__result__keyword">すべてのジム</span>
+        <span class="searched_heading-box__result__count">10件</span>
+    </div>
+    <div class="searched_heading-box__sort">
+        <select name="sort_order" class="sort-order searched_heading-box__sort__select">
+            <option value="location.pathname" name="location.pathname">並び替え</option>
+            <option value="rate-desc">評価の高い順</option>
+            <option value="woms-desc">口コミの多い順</option>
+            <option value="clips-desc">保存済みの多い順</option>
+            <option value="created_at-desc">登録順</option>
+            <option value="created_at-desc">新着順</option>
+        </select>
+    </div>
+</div>
