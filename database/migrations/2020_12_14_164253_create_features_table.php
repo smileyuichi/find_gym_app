b749307e234b5f5a808b6c6fd0dfd56aac593b47
smileyuichi <smile.yuichi.1013@gmail.com>
2020-12-14 22:31:20 +0900

    implement:gymとfeatureのリレーションを実装

--- database/migrations/2020_12_14_164253_create_features_table.php
+++ database/migrations/2020_12_14_164253_create_features_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateGymsTable extends Migration
+class CreateFeaturesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,9 @@ class CreateGymsTable extends Migration
      */
     public function up()
     {
-        Schema::create('gyms', function (Blueprint $table) {
+        Schema::create('features', function (Blueprint $table) {
             $table->id()->autoIncrement();
-            $table->string('name');
-            $table->text('outline');
-            $table->string('postal_code');
-            $table->string('address');
-            $table->float('latitude');
-            $table->float('longtitude');
+            $table->string('name',255);
             $table->timestamps();
         });
     }
@@ -32,6 +27,6 @@ class CreateGymsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('gyms');
+        Schema::dropIfExists('features');
     }
 }
