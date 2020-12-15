a3672274195d11e77bee238cc28229add610a0b0
smileyuichi <smile.yuichi.1013@gmail.com>
2020-12-15 17:35:05 +0900

    add:ジムの名前あるいはジムの特徴で検索が可能

--- app/Http/Controllers/GymController.php
+++ app/Http/Controllers/GymController.php
@@ -11,14 +11,23 @@ class GymController extends Controller
 {
     public function index(Request $request)
     {
-        if(isset($request->id))
+
+        if(isset($request->name))
         {
-            $param = ['id' => $request->id];
-            $items = DB::select('select * from gyms where id = :id',$param);
-        } else {
-            $items = DB::table("gyms")->paginate(5);
-        }
+            $param = ['gym_name' => $request->name];
+            // ジムの名前で検索する
+            $items = DB::table('gyms')->where('name','like','%'. $param['gym_name'] . '%')->paginate(5);
+        }elseif(isset($request->feature))
+        {
+            $param = ['feature' => $request->feature];
+            // ジムの特徴で検索する
+            $items = DB::table('gyms')->join('feature_gym','gyms.id','=','feature_gym.gym_id')->join('features','features.id','=','feature_gym.feature_id')->select('gyms.*','features.id')->where('features.name','like', '%'.$param['feature'].'%')->paginate(5);
         
+        }else{
+            var_dump('miss');
+            $items = DB::table('gyms')->paginate(5);
+        }
+
         return view('gyms.index', ['items' => $items]);
     }
 
